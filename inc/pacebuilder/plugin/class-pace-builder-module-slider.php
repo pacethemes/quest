d3ab35818ea72d07c188f077f9ad2c1cbef0f3ed
Ravi Chandra <ravi@pacethemes.com>
2016-10-25 05:29:31 +0530

    Theme check fixes

--- inc/pacebuilder/plugin/class-pace-builder-module-slider.php
+++ inc/pacebuilder/plugin/class-pace-builder-module-slider.php
@@ -22,10 +22,10 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 		public function __construct() {
 			parent::__construct();
 			$this->icon        = 'dashicons-images-alt';
-			$this->label       = __( 'Slider', 'pace-builder' );
-			$this->description = __( 'A simple Image Slider', 'pace-builder' );
+			$this->label       = __( 'Slider', 'quest' );
+			$this->description = __( 'A simple Image Slider', 'quest' );
 			$this->has_items   = true;
-			$this->item_label  = __( 'Slide', 'pace-builder' );
+			$this->item_label  = __( 'Slide', 'quest' );
 		}
 
 		/**
@@ -41,20 +41,20 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 					'max'	  => 1500,
 					'step'	  => 5,
 					'unit'	  => '',
-					'label'   => __( 'Height', 'pace-builder' )
+					'label'   => __( 'Height', 'quest' )
 				),
 				'fullscreen' => array(
 					'type'    => 'select',
 					'default' => 'no',
-					'label'   => __( 'Fullscreen', 'pace-builder' ),
-					'desc'    => __( 'If set to FullScreen Slider height will be ignored and the slider will always be 100% to the browser viewport', 'pace-builder' ),
+					'label'   => __( 'Fullscreen', 'quest' ),
+					'desc'    => __( 'If set to FullScreen Slider height will be ignored and the slider will always be 100% to the browser viewport', 'quest' ),
 					'options' => $this->yes_no_option
 				),
 				'autoplay' => array(
 					'type'    => 'select',
 					'default' => 'yes',
-					'label'   => __( 'AutoPlay', 'pace-builder' ),
-					'desc'    => __( 'Do you want to enable AutoPlay for this slider ? If autoplay is turned on, you can control the interval using Autoplay Interval option.', 'pace-builder' ),
+					'label'   => __( 'AutoPlay', 'quest' ),
+					'desc'    => __( 'Do you want to enable AutoPlay for this slider ? If autoplay is turned on, you can control the interval using Autoplay Interval option.', 'quest' ),
 					'options' => $this->yes_no_option
 				),
 				'interval' => array(
@@ -64,17 +64,17 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 					'max'	  => 60000,
 					'step'	  => 10,
 					'unit'	  => '',
-					'label'   => __( 'AutoPlay Interval', 'pace-builder' ),
-					'desc'    => __( 'Interval between playing the next slide, specify the time in milliseconds.', 'pace-builder' )
+					'label'   => __( 'AutoPlay Interval', 'quest' ),
+					'desc'    => __( 'Interval between playing the next slide, specify the time in milliseconds.', 'quest' )
 				),
 				'animation' => array(
 					'type'    => 'select',
 					'default' => 'slit',
-					'label'   => __( 'Transition', 'pace-builder' ),
-					'desc'    => __( 'Transition type for the slider, Slit or Fade ?', 'pace-builder' ),
+					'label'   => __( 'Transition', 'quest' ),
+					'desc'    => __( 'Transition type for the slider, Slit or Fade ?', 'quest' ),
 					'options' => array(
-							'slit'	=> __( 'Slit', 'pace-builder' ),
-							'fade'	=> __( 'Fade', 'pace-builder' )
+							'slit'	=> __( 'Slit', 'quest' ),
+							'fade'	=> __( 'Fade', 'quest' )
 						)
 				),
 				'speed' => array(
@@ -84,8 +84,8 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 					'max'	  => 5000,
 					'step'	  => 5,
 					'unit'	  => '',
-					'label'   => __( 'Transition speed', 'pace-builder' ),
-					'desc'    => __( 'Speed of the CSS3 slide transitions, specify the time in milliseconds.', 'pace-builder' )
+					'label'   => __( 'Transition speed', 'quest' ),
+					'desc'    => __( 'Speed of the CSS3 slide transitions, specify the time in milliseconds.', 'quest' )
 				),
 			);
 		}
@@ -98,76 +98,76 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 			return array(
 				'bg_image'   => array(
 					'type'  => 'image',
-					'label' => __( 'Image', 'pace-builder' )
+					'label' => __( 'Image', 'quest' )
 				),
 				'bg_pos_x' => array(
 					'type'    => 'select',
 					'default' => 'center',
-					'label'   => __( 'Image Position - Horizontal', 'pace-builder' ),
-					'desc'    => __( 'Horizontal position of the Image, if the image width is more than the slider width then the image will be positioned as per this setting.', 'pace-builder' ),
+					'label'   => __( 'Image Position - Horizontal', 'quest' ),
+					'desc'    => __( 'Horizontal position of the Image, if the image width is more than the slider width then the image will be positioned as per this setting.', 'quest' ),
 					'options' => array(
-							'center'	=> __( 'Center', 'pace-builder' ),
-							'left'	=> __( 'Left', 'pace-builder' ),
-							'right'	=> __( 'Right', 'pace-builder' ),
+							'center'	=> __( 'Center', 'quest' ),
+							'left'	=> __( 'Left', 'quest' ),
+							'right'	=> __( 'Right', 'quest' ),
 						)
 				),
 				'bg_pos_y' => array(
 					'type'    => 'select',
 					'default' => 'center',
-					'label'   => __( 'Image Position - Vertical', 'pace-builder' ),
-					'desc'    => __( 'Vertical position of the Image, if the image height is more than the slider height then the image will be positioned as per this setting.', 'pace-builder' ),
+					'label'   => __( 'Image Position - Vertical', 'quest' ),
+					'desc'    => __( 'Vertical position of the Image, if the image height is more than the slider height then the image will be positioned as per this setting.', 'quest' ),
 					'options' => array(
-							'center'	=> __( 'Center', 'pace-builder' ),
-							'top'	=> __( 'Top', 'pace-builder' ),
-							'bottom'	=> __( 'Bottom', 'pace-builder' ),
+							'center'	=> __( 'Center', 'quest' ),
+							'top'	=> __( 'Top', 'quest' ),
+							'bottom'	=> __( 'Bottom', 'quest' ),
 						)
 				),
 				'bg_color'        => array(
 					'type'    => 'color',
 					'default' => '#dddddd',
-					'label'   => __( 'Slide Background Color', 'pace-builder' )
+					'label'   => __( 'Slide Background Color', 'quest' )
 				),
 				'content_pos_x' => array(
 					'type'    => 'select',
 					'default' => 'center',
-					'label'   => __( 'Content Position - Horizontal', 'pace-builder' ),
-					'desc'    => __( 'Horizontal position of the Image, if the image width is more than the slider width then the image will be positioned as per this setting.', 'pace-builder' ),
+					'label'   => __( 'Content Position - Horizontal', 'quest' ),
+					'desc'    => __( 'Horizontal position of the Image, if the image width is more than the slider width then the image will be positioned as per this setting.', 'quest' ),
 					'options' => array(
-							'center'	=> __( 'Center', 'pace-builder' ),
-							'left'	=> __( 'Left', 'pace-builder' ),
-							'right'	=> __( 'Right', 'pace-builder' ),
+							'center'	=> __( 'Center', 'quest' ),
+							'left'	=> __( 'Left', 'quest' ),
+							'right'	=> __( 'Right', 'quest' ),
 						)
 				),
 				'content_pos_y' => array(
 					'type'    => 'select',
 					'default' => 'center',
-					'label'   => __( 'Content Position - Vertical', 'pace-builder' ),
-					'desc'    => __( 'Vertical position of the Image, if the image height is more than the slider height then the image will be positioned as per this setting.', 'pace-builder' ),
+					'label'   => __( 'Content Position - Vertical', 'quest' ),
+					'desc'    => __( 'Vertical position of the Image, if the image height is more than the slider height then the image will be positioned as per this setting.', 'quest' ),
 					'options' => array(
-							'center'	=> __( 'Center', 'pace-builder' ),
-							'top'	=> __( 'Top', 'pace-builder' ),
-							'bottom'	=> __( 'Bottom', 'pace-builder' ),
+							'center'	=> __( 'Center', 'quest' ),
+							'top'	=> __( 'Top', 'quest' ),
+							'bottom'	=> __( 'Bottom', 'quest' ),
 						)
 				),
 				'heading'   => array(
 					'type'  => 'text',
-					'label' => __( 'Slide Heading', 'pace-builder' ),
-					'desc'    => __( 'The heading for the slide, this will be the main heading/title displayed in the slide frontend.', 'pace-builder' ),
+					'label' => __( 'Slide Heading', 'quest' ),
+					'desc'    => __( 'The heading for the slide, this will be the main heading/title displayed in the slide frontend.', 'quest' ),
 				),
 				'heading_color'        => array(
 					'type'    => 'color',
 					'default' => '',
-					'label'   => __( 'Slide Heading Text Color', 'pace-builder' )
+					'label'   => __( 'Slide Heading Text Color', 'quest' )
 				),
 				'heading_bg_color'        => array(
 					'type'    => 'color',
 					'default' => '',
-					'label'   => __( 'Slide Heading Background Color', 'pace-builder' )
+					'label'   => __( 'Slide Heading Background Color', 'quest' )
 				),
 				'heading_size'      => array(
 					'type'  => 'slider',
 					'default' => 42,
-					'label' => __( 'Slide Heading Text Size', 'pace-builder' ),
+					'label' => __( 'Slide Heading Text Size', 'quest' ),
 					'max'   => 72,
 					'min'   => 10,
 					'step'  => 1,
@@ -175,23 +175,23 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 				),
 				'content'      => array(
 					'type'  => 'tinymce',
-					'label' => __( 'Slide Text', 'pace-builder' ),
-					'desc'  => __( 'Content for the slide, this will be displayed in the front end below the heading', 'pace-builder' )
+					'label' => __( 'Slide Text', 'quest' ),
+					'desc'  => __( 'Content for the slide, this will be displayed in the front end below the heading', 'quest' )
 				),
 				'text_color'        => array(
 					'type'    => 'color',
 					'default' => '',
-					'label'   => __( 'Slide Text Color', 'pace-builder' )
+					'label'   => __( 'Slide Text Color', 'quest' )
 				),
 				'text_bg_color'        => array(
 					'type'    => 'color',
 					'default' => '',
-					'label'   => __( 'Slide Text Background Color', 'pace-builder' )
+					'label'   => __( 'Slide Text Background Color', 'quest' )
 				),
 				'text_size'      => array(
 					'type'  => 'slider',
 					'default' => 18,
-					'label' => __( 'Slide Text Size', 'pace-builder' ),
+					'label' => __( 'Slide Text Size', 'quest' ),
 					'max'   => 24,
 					'min'   => 10,
 					'step'  => 1,
@@ -200,18 +200,18 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 				'orientation' => array(
 					'type'    => 'select',
 					'default' => 'vertical',
-					'label'   => __( 'Slice Orientation', 'pace-builder' ),
-					'desc'    => __( 'Should the slices split vertically or horizontally ?', 'pace-builder' ),
+					'label'   => __( 'Slice Orientation', 'quest' ),
+					'desc'    => __( 'Should the slices split vertically or horizontally ?', 'quest' ),
 					'options' => array(
-							'vertical'	=> __( 'Vertical', 'pace-builder' ),
-							'horizontal'	=> __( 'Horizontal', 'pace-builder' )
+							'vertical'	=> __( 'Vertical', 'quest' ),
+							'horizontal'	=> __( 'Horizontal', 'quest' )
 						)
 				),
 				'slice1_rotation'      => array(
 					'type'  => 'slider',
 					'default' => 10,
-					'label' => __( 'Slice 1 Rotation', 'pace-builder' ),
-					'desc'  => __( 'Amount of rotation in degrees for the first slice.', 'pace-builder' ),
+					'label' => __( 'Slice 1 Rotation', 'quest' ),
+					'desc'  => __( 'Amount of rotation in degrees for the first slice.', 'quest' ),
 					'max'   => 40,
 					'min'   => -40,
 					'step'  => 1,
@@ -220,8 +220,8 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 				'slice2_rotation'      => array(
 					'type'  => 'slider',
 					'default' => -15,
-					'label' => __( 'Slice 2 Rotation', 'pace-builder' ),
-					'desc'  => __( 'Amount of rotation in degrees for the second slice.', 'pace-builder' ),
+					'label' => __( 'Slice 2 Rotation', 'quest' ),
+					'desc'  => __( 'Amount of rotation in degrees for the second slice.', 'quest' ),
 					'max'   => 40,
 					'min'   => -40,
 					'step'  => 1,
@@ -230,8 +230,8 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 				'slice1_scale'      => array(
 					'type'  => 'slider',
 					'default' => 1.5,
-					'label' => __( 'Slice 1 Scale', 'pace-builder' ),
-					'desc'  => __( 'How big should the slice 1 scale ?', 'pace-builder' ),
+					'label' => __( 'Slice 1 Scale', 'quest' ),
+					'desc'  => __( 'How big should the slice 1 scale ?', 'quest' ),
 					'max'   => 3,
 					'min'   => 1,
 					'step'  => 0.1,
@@ -240,8 +240,8 @@ if ( ! class_exists( 'PTPB_Module_Slider' ) ) :
 				'slice2_scale'      => array(
 					'type'  => 'slider',
 					'default' => 1.5,
-					'label' => __( 'Slice 2 Scale', 'pace-builder' ),
-					'desc'  => __( 'How big should the slice 2 scale ?', 'pace-builder' ),
+					'label' => __( 'Slice 2 Scale', 'quest' ),
+					'desc'  => __( 'How big should the slice 2 scale ?', 'quest' ),
 					'max'   => 3,
 					'min'   => 1,
 					'step'  => 0.1,
--- pace-builder/layouts/layouts.php
+++ pace-builder/layouts/layouts.php
@@ -21,7 +21,7 @@ if( ! function_exists( 'quest_ptpb_prebuilt_layouts' ) ) :
 				'tab_pane'	=> 'Quest Prebuilt',
 				'thumb'		=> 'http://demo.pacethemes.com/pace-builder/wp-content/uploads/sites/6/2016/06/app-landing-350x300.jpg',
 				'preview'	=> 'http://demo.pacethemes.com/pace-builder/layouts/app-landing-page/',
-				'layout'	=> json_decode( file_get_contents( $path . '/agency.json' ), true )
+				'layout'	=> json_decode( quest_read_layout_file( $path . 'agency.json' ), true )
 			);
 
 		return $layouts;
@@ -30,3 +30,16 @@ if( ! function_exists( 'quest_ptpb_prebuilt_layouts' ) ) :
 endif;
 
 add_filter( 'ptpb_prebuilt_layouts', 'quest_ptpb_prebuilt_layouts' );
+
+if( ! function_exists( 'quest_read_layout_file' ) ) :
+	/**
+	 * Read a layout json file and return its content
+	 *
+	 * @param $file
+	 */
+	function quest_read_layout_file( $file ) {
+		WP_Filesystem();
+		global $wp_filesystem;
+		return $wp_filesystem->get_contents( $file );
+	}
+endif;
